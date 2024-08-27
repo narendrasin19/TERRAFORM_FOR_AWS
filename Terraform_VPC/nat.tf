9fcbc9d4a241ff195285677e5faa0a1747f1a32d
Narendra <narendra.sin19@gmail.com>
2024-08-27 12:41:25 +0530

    Create nat.tf

---
+++ Terraform_VPC/nat.tf
@@ -0,0 +1,39 @@
+# Define External IP and NAT Gateway
+resource "aws_eip" "levelup-nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "levelup-nat-gw" {
+  allocation_id = aws_eip.levelup-nat.id
+  subnet_id     = aws_subnet.levelupvpc-public-1.id
+  depends_on    = [aws_internet_gateway.levelup-gw]
+}
+
+# Route Table for Private Subnets
+resource "aws_route_table" "levelup-private" {
+  vpc_id = aws_vpc.levelupvpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.levelup-nat-gw.id
+  }
+
+  tags = {
+    Name = "levelup-private"
+  }
+}
+
+# Route Table Associations for Private Subnets
+resource "aws_route_table_association" "level-private-1-a" {
+  subnet_id      = aws_subnet.levelupvpc-private-1.id
+  route_table_id = aws_route_table.levelup-private.id
+}
+
+resource "aws_route_table_association" "level-private-1-b" {
+  subnet_id      = aws_subnet.levelupvpc-private-2.id
+  route_table_id = aws_route_table.levelup-private.id
+}
+
+resource "aws_route_table_association" "level-private-1-c" {
+  subnet_id      = aws_subnet.levelupvpc-private-3.id
+  route_table_id = aws_route_table.levelup-private.id
+}
