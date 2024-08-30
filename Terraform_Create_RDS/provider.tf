36e498c49ae77be61525a7982d0339347ee7b19d
Narendra <narendra.sin19@gmail.com>
2024-08-30 12:14:27 +0530

    Create provider.tf

---
+++ Terraform_Create_RDS/provider.tf
@@ -0,0 +1,12 @@
+# Configure the AWS provider with access credentials and region
+provider "aws" {
+  
+  # Access key for AWS account (retrieved from variable 'AWS_ACCESS_KEY')
+  access_key = var.AWS_ACCESS_KEY
+  
+  # Secret key for AWS account (retrieved from variable 'AWS_SECRET_KEY')
+  secret_key = var.AWS_SECRET_KEY
+  
+  # AWS region where resources will be created (retrieved from variable 'AWS_REGION')
+  region = var.AWS_REGION
+}
