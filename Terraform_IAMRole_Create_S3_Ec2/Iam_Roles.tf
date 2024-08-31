33aad28afc95042ec9bb5c2948cb342d3506e906
Narendra <narendra.sin19@gmail.com>
2024-08-31 11:52:40 +0530

    Update Iam_Roles.tf

--- Terraform_IAMRole_Create_S3_Ec2/Iam_Roles.tf
+++ Terraform_IAMRole_Create_S3_Ec2/Iam_Roles.tf
@@ -1,9 +1,6 @@
-# Iam_Roles.tf 
-# 1. Define an IAM role for accessing S3
+# 4. Define an IAM Role for S3 Access
 resource "aws_iam_role" "s3_levelupbucket_role" {
   name               = "s3-levelupbucket-role"
-  
-  # 1.1. Assume role policy that allows EC2 instances to assume this role
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -13,20 +10,17 @@ resource "aws_iam_role" "s3_levelupbucket_role" {
       "Principal": {
         "Service": "ec2.amazonaws.com"
       },
-      "Action": "sts:AssumeRole",
-      "Sid": ""
+      "Action": "sts:AssumeRole"
     }
   ]
 }
 EOF
 }
 
-# 2. Define a policy to attach to the IAM role for S3 access
-resource "aws_iam_role_policy" "s3_levelupmybucket_role_policy" {
-  name   = "s3-levelupmybucket-role-policy"
+# 5. Define a Policy to Attach to the IAM Role for S3 Access
+resource "aws_iam_role_policy" "s3_levelupbucket_role_policy" {
+  name   = "s3-levelupbucket-role-policy"
   role   = aws_iam_role.s3_levelupbucket_role.id
-
-  # 2.1. Policy granting full access to a specific S3 bucket and its objects
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -37,8 +31,8 @@ resource "aws_iam_role_policy" "s3_levelupmybucket_role_policy" {
         "s3:*"
       ],
       "Resource": [
-        "arn:aws:s3:::levelup-bucket-141",     # ARN of the S3 bucket
-        "arn:aws:s3:::levelup-bucket-141/*"    # ARN of objects within the bucket
+        "arn:aws:s3:::levelup-bucket-141",
+        "arn:aws:s3:::levelup-bucket-141/*"
       ]
     }
   ]
@@ -46,8 +40,8 @@ resource "aws_iam_role_policy" "s3_levelupmybucket_role_policy" {
 EOF
 }
 
-# 3. Define an instance profile to use the IAM role
+# 6. Define an Instance Profile to Use the IAM Role
 resource "aws_iam_instance_profile" "s3_levelupbucket_role_instanceprofile" {
   name = "s3-levelupbucket-role"
-  role = aws_iam_role.s3_levelupbucket_role.name  # Reference to the IAM role
+  role = aws_iam_role.s3_levelupbucket_role.name
 }
