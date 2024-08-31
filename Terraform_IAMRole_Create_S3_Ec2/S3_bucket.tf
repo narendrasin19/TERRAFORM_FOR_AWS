964ba4a6038a94ac7aeeca108b030dc26e240c1a
Narendra <narendra.sin19@gmail.com>
2024-08-31 11:57:13 +0530

    Update S3_bucket.tf

--- Terraform_IAMRole_Create_S3_Ec2/S3_bucket.tf
+++ Terraform_IAMRole_Create_S3_Ec2/S3_bucket.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "levelup_s3bucket" {
 }
 
 # 3. Create an ACL for the AWS S3 Bucket
-resource "aws_s3_bucket_acl" "levelup_s3bucket_acl" {
-  bucket = aws_s3_bucket.levelup_s3bucket.id  # 3.1. Reference to the S3 bucket
-  acl    = "private"                          # 3.2. Access control list set to private
-}
+#resource "aws_s3_bucket_acl" "levelup_s3bucket_acl" {
+#  bucket = aws_s3_bucket.levelup_s3bucket.id  # 3.1. Reference to the S3 bucket
+#  acl    = "private"                          # 3.2. Access control list set to private
+#}
