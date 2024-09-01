7c568de498ce3e9a38f71a4898fcc1bcd5967a5a
Narendra <narendra.sin19@gmail.com>
2024-09-01 12:12:44 +0530

    Create provider.tf

---
+++ TERRAFORM_MODULES_FOR_AWS/provider.tf
@@ -0,0 +1,11 @@
+# This file can be used to define your provider configurations separately if you prefer to organize your code that way.
+# provider.tf
+
+# This file can be used to define your provider configurations separately if you prefer to organize your code that way.
+# provider.tf
+
+provider "aws" {
+  access_key = "xxx"
+  secret_key = "xxxxx"
+  region     = "us-east-2"
+}
